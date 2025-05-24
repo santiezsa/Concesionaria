e3fe132d8cf642d0de660ac723970225549414d7
sasanchez <santisanchez98@hotmail.com>
2025-05-23 22:15:06 -0300

    minor changes

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
+/*
+cd C:\0repos\Concesionaria  # Ir al directorio del proyecto
+git init                     # Inicializar Git en la carpeta
+git remote add origin https://github.com/santiezsa/Concesionaria.git  # Vincular el remoto
+git add .                     # Agregar archivos
+git commit -m "Inicializando repositorio"  # Guardar cambios
+git push -u origin main       # Subir el código por primera vez
+*/
+
 #include <iostream>
 
 using namespace std;
