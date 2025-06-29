28fde4e3e9dd452534efacc261b88343f3624048
sasanchez <santisanchez98@hotmail.com>
2025-06-29 16:17:53 -0300

    Agrego menu consultas (pendiente autos)

--- Menu.h
+++ Menu.h
@@ -35,6 +35,10 @@ class Menu
         void mostrarMenuListadosAutosNuevos();
         void mostrarMenuListadosAutosUsados();
 
+        /// Consultas
+        void mostrarMenuConsultasVentas();
+        void mostrarMenuConsultasAutos();
+
     private:
 };
 
